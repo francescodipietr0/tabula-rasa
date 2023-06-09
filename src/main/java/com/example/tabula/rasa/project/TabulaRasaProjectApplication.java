225b0b0f4ff946860fe028d60952449f15a8b098
Di Pietro <francesco.di.pietro@accenture.com>
2023-06-09 12:32:37 +0200

    project imported

---
+++ src/main/java/com/example/tabula/rasa/project/TabulaRasaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabula.rasa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabulaRasaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabulaRasaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tabula/rasa/project/TabulaRasaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabula.rasa.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabulaRasaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
